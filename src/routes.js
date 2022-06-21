cd54e803df1600338d75765525e062c6eaccc16f
Nat-1501 <natalisoares90@gmail.com>
2022-06-21 10:17:23 -0300

    Criada as routes

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+function Router() {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Router></Router>
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Router;
